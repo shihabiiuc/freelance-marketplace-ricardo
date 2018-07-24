461dcd283ce8969ebef83f4d64b5a8c45ab0ae6d
091529A091529b <shihabdinajpur@gmail.com>
2018-07-24 21:27:51 +0600

    Dynamic project categories

--- wp-content/themes/freelancemarketplacericardo-child/front-page.php
+++ wp-content/themes/freelancemarketplacericardo-child/front-page.php
@@ -4,210 +4,215 @@
  * User: Shihab
  * Date: 24/07/2018
  * Time: 16:57
+ * Template Name: Ricardo Home Page
  */
-?>
-
-<?php
-/**
- * Template Name: Front page
- */
-
 get_header();
 global $user_ID;
 ?>
 
-<h1>Hello world</h1>
+<!--Project categories nav-->
+
 <?php
-$category_project_selected = '';
-if ( isset( $_GET['category_project'] ) && $_GET['category_project'] != '' ) {
-	$category_project_selected = $_GET['category_project'];
-}
+$terms = get_terms('project_category', array(
+	'orderby'   => 'name',
+	'order'     => 'ASC',
+	'hide_empty' => true,
+	'number' => 6,
+)); ?>
 
-?>
+<nav id="catNav" class="navbar navbar-default">
+    <div class="container-fluid">
 
-<div class="fre-project-filter-box">
-	<script type="data/json" id="search_data">
-            <?php
-		$search_data = $_POST;
-		echo json_encode( $search_data );
-		?>
-
-    </script>
-	<div class="project-filter-header visible-sm visible-xs">
-		<a class="project-filter-title" href=""><?php _e( 'Advance search', ET_DOMAIN ); ?></a>
-	</div>
-	<div class="fre-project-list-filter">
-		<form>
-			<div class="row">
-
-				<div class="col-md-4">
-					<div class="fre-input-field">
-						<label for="project_category"
-							   class="fre-field-title"><?php _e( 'Category', ET_DOMAIN ); ?></label>
-						<?php ae_tax_dropdown( 'project_category', array(
-								'attr'            => 'data-chosen-width="100%" data-chosen-disable-search="" data-placeholder="' . __( "Select categories", ET_DOMAIN ) . '"',
-								'show_option_all' => __( "Select category", ET_DOMAIN ),
-								'class'           => 'fre-chosen-single',
-								'hide_empty'      => false,
-								'hierarchical'    => true,
-								'selected'        => $category_project_selected,
-								'id'              => 'project_category',
-								'value'           => 'slug',
-							)
-						); ?>
-					</div>
-				</div>
-
-			</div>
-
-		</form>
-	</div>
-</div>
-<h1>Hello world</h1>
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand visible-xs" href="#">Top Categories</a>
+        </div>
 
 
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
 
+                <?php
+                    foreach ($terms as $term) {
+                        $term_link = get_term_link($term);
+                        if (is_wp_error($term_link)) {
+                            continue;
+                        }
+                        echo '<li><a href="' . esc_url($term_link) . '">' . $term->name . '</a></li>';
+                    }
+				?>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="<?php echo site_url('/project_category/web-search/'); ?>">View All Categories</a></li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
+<!--Project categories nav end-->
 
 
 <!-- Block Banner -->
-<div class="fre-background" id="background_banner" style="background-image: url('<?php echo get_theme_mod("background_banner") ? get_theme_mod("background_banner") : get_template_directory_uri()."/img/fre-bg.png";?>');">
-	<div class="fre-bg-content">
-		<div class="container">
-			<h1 id="title_banner"><?php echo get_theme_mod("title_banner") ? get_theme_mod("title_banner") : __("Find perfect freelancers for your projects or Look for freelance jobs online?", ET_DOMAIN);?></h1>
-			<?php if(!is_user_logged_in()){ ?>
-				<?php if(!fre_check_register()){ ?>
-					<a class="fre-btn primary-bg-color" href="<?php echo get_post_type_archive_link( PROFILE ); ?>"><?php _e('Find Freelancers', ET_DOMAIN);?></a>
-					<a class="fre-btn primary-bg-color" href="<?php echo get_post_type_archive_link( PROJECT ); ?>"><?php _e('Find Projects', ET_DOMAIN);?></a>
-				<?php }else{ ?>
-					<a class="fre-btn primary-bg-color" href="<?php echo et_get_page_link('register', array("role"=>'employer')); ?>"><?php _e('Hire Freelancer', ET_DOMAIN);?></a>
-					<a class="fre-btn primary-bg-color" href="<?php echo et_get_page_link('register', array("role"=>'freelancer')); ?>"><?php _e('Apply as Freelancer', ET_DOMAIN);?></a>
+<div class="fre-background" id="background_banner"
+     style="background-image: url('<?php echo get_theme_mod("background_banner") ? get_theme_mod("background_banner") : get_template_directory_uri() . "/img/fre-bg.png"; ?>');">
+    <div class="fre-bg-content">
+        <div class="container">
+            <h1 id="title_banner"><?php echo get_theme_mod("title_banner") ? get_theme_mod("title_banner") : __("Find perfect freelancers for your projects or Look for freelance jobs online?", ET_DOMAIN); ?></h1>
+			<?php if (!is_user_logged_in()) { ?>
+				<?php if (!fre_check_register()) { ?>
+                    <a class="fre-btn primary-bg-color"
+                       href="<?php echo get_post_type_archive_link(PROFILE); ?>"><?php _e('Find Freelancers', ET_DOMAIN); ?></a>
+                    <a class="fre-btn primary-bg-color"
+                       href="<?php echo get_post_type_archive_link(PROJECT); ?>"><?php _e('Find Projects', ET_DOMAIN); ?></a>
+				<?php } else { ?>
+                    <a class="fre-btn primary-bg-color"
+                       href="<?php echo et_get_page_link('register', array("role" => 'employer')); ?>"><?php _e('Hire Freelancer', ET_DOMAIN); ?></a>
+                    <a class="fre-btn primary-bg-color"
+                       href="<?php echo et_get_page_link('register', array("role" => 'freelancer')); ?>"><?php _e('Apply as Freelancer', ET_DOMAIN); ?></a>
 				<?php } ?>
 
-			<?php }else{ ?>
-				<?php if(ae_user_role($user_ID) == FREELANCER){ ?>
-					<a class="fre-btn primary-bg-color" href="<?php echo get_post_type_archive_link( PROJECT ); ?>"><?php _e('Find Projects', ET_DOMAIN);?></a>
-					<a class="fre-btn primary-bg-color" href="<?php echo et_get_page_link('profile'); ?>"><?php _e('Update Profile', ET_DOMAIN);?></a>
-				<?php }else{ ?>
-					<a class="fre-btn primary-bg-color" href="<?php echo et_get_page_link('submit-project'); ?>"><?php _e('Post a Project', ET_DOMAIN);?></a>
-					<a class="fre-btn primary-bg-color" href="<?php echo get_post_type_archive_link( PROFILE ); ?>"><?php _e('Find Freelancers', ET_DOMAIN);?></a>
+			<?php } else { ?>
+				<?php if (ae_user_role($user_ID) == FREELANCER) { ?>
+                    <a class="fre-btn primary-bg-color"
+                       href="<?php echo get_post_type_archive_link(PROJECT); ?>"><?php _e('Find Projects', ET_DOMAIN); ?></a>
+                    <a class="fre-btn primary-bg-color"
+                       href="<?php echo et_get_page_link('profile'); ?>"><?php _e('Update Profile', ET_DOMAIN); ?></a>
+				<?php } else { ?>
+                    <a class="fre-btn primary-bg-color"
+                       href="<?php echo et_get_page_link('submit-project'); ?>"><?php _e('Post a Project', ET_DOMAIN); ?></a>
+                    <a class="fre-btn primary-bg-color"
+                       href="<?php echo get_post_type_archive_link(PROFILE); ?>"><?php _e('Find Freelancers', ET_DOMAIN); ?></a>
 				<?php } ?>
 			<?php } ?>
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
 <!-- Block Banner -->
 <!-- Block How Work -->
 <div class="fre-how-work">
-	<div class="container">
-		<h2 id="title_work"><?php echo get_theme_mod("title_work") ? get_theme_mod("title_work") : __('How FreelanceEngine works?', ET_DOMAIN);?></h2>
-		<div class="row">
-			<div class="col-lg-3 col-sm-6">
-				<div class="fre-work-block">
+    <div class="container">
+        <h2 id="title_work"><?php echo get_theme_mod("title_work") ? get_theme_mod("title_work") : __('How FreelanceEngine works?', ET_DOMAIN); ?></h2>
+        <div class="row">
+            <div class="col-lg-3 col-sm-6">
+                <div class="fre-work-block">
 					<span>
-						<img src="<?php echo get_theme_mod('img_work_1') ? get_theme_mod('img_work_1') : get_template_directory_uri().'/img/1.png';?>" id="img_work_1" alt="">
+						<img src="<?php echo get_theme_mod('img_work_1') ? get_theme_mod('img_work_1') : get_template_directory_uri() . '/img/1.png'; ?>"
+                             id="img_work_1" alt="">
 					</span>
-					<p id="desc_work_1"><?php echo get_theme_mod("desc_work_1") ? get_theme_mod("desc_work_1") : __('Post projects to tell us what you need done', ET_DOMAIN);?></p>
-				</div>
-			</div>
-			<div class="col-lg-3 col-sm-6">
-				<div class="fre-work-block">
+                    <p id="desc_work_1"><?php echo get_theme_mod("desc_work_1") ? get_theme_mod("desc_work_1") : __('Post projects to tell us what you need done', ET_DOMAIN); ?></p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="fre-work-block">
 					<span>
-						<img src="<?php echo get_theme_mod('img_work_2') ? get_theme_mod('img_work_2') : get_template_directory_uri().'/img/2.png';?>" id="img_work_2" alt="">
+						<img src="<?php echo get_theme_mod('img_work_2') ? get_theme_mod('img_work_2') : get_template_directory_uri() . '/img/2.png'; ?>"
+                             id="img_work_2" alt="">
 					</span>
-					<p id="desc_work_2"><?php echo get_theme_mod("desc_work_2") ? get_theme_mod("desc_work_2") : __('Browse profiles, reviews, then hire your most favorite and start project', ET_DOMAIN);?></p>
-				</div>
-			</div>
-			<div class="col-lg-3 col-sm-6">
-				<div class="fre-work-block">
+                    <p id="desc_work_2"><?php echo get_theme_mod("desc_work_2") ? get_theme_mod("desc_work_2") : __('Browse profiles, reviews, then hire your most favorite and start project', ET_DOMAIN); ?></p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="fre-work-block">
 					<span>
-						<img src="<?php echo get_theme_mod('img_work_3') ? get_theme_mod('img_work_3') : get_template_directory_uri().'/img/3.png';?>" id="img_work_3" alt="">
+						<img src="<?php echo get_theme_mod('img_work_3') ? get_theme_mod('img_work_3') : get_template_directory_uri() . '/img/3.png'; ?>"
+                             id="img_work_3" alt="">
 					</span>
-					<p id="desc_work_3"><?php echo get_theme_mod("desc_work_3") ? get_theme_mod("desc_work_3") : __('Use FreelanceEngine platform to chat and share files', ET_DOMAIN);?></p>
-				</div>
-			</div>
-			<div class="col-lg-3 col-sm-6">
-				<div class="fre-work-block">
+                    <p id="desc_work_3"><?php echo get_theme_mod("desc_work_3") ? get_theme_mod("desc_work_3") : __('Use FreelanceEngine platform to chat and share files', ET_DOMAIN); ?></p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="fre-work-block">
 					<span>
-						<img src="<?php echo get_theme_mod('img_work_4') ? get_theme_mod('img_work_4') : get_template_directory_uri().'/img/4.png';?>" id="img_work_4" alt="">
+						<img src="<?php echo get_theme_mod('img_work_4') ? get_theme_mod('img_work_4') : get_template_directory_uri() . '/img/4.png'; ?>"
+                             id="img_work_4" alt="">
 					</span>
-					<p id="desc_work_4"><?php echo get_theme_mod("desc_work_4") ? get_theme_mod("desc_work_4") : __('With our protection, money is only paid for work you authorize', ET_DOMAIN);?></p>
-				</div>
-			</div>
-		</div>
-	</div>
+                    <p id="desc_work_4"><?php echo get_theme_mod("desc_work_4") ? get_theme_mod("desc_work_4") : __('With our protection, money is only paid for work you authorize', ET_DOMAIN); ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- Block How Work -->
 <!-- List Profiles -->
 <div class="fre-perfect-freelancer">
-	<div class="container">
-		<h2 id="title_freelance"><?php echo get_theme_mod("title_freelance") ? get_theme_mod("title_freelance") : __('Find perfect freelancers for your projects', ET_DOMAIN);?></h2>
-		<?php get_template_part( 'home-list', 'profiles' );?>
-		<div class="fre-perfect-freelancer-more">
-			<a class="fre-btn-o primary-color" href="<?php echo get_post_type_archive_link( PROFILE ); ?>"><?php _e('See all freelancers', ET_DOMAIN);?></a>
-		</div>
-	</div>
+    <div class="container">
+        <h2 id="title_freelance"><?php echo get_theme_mod("title_freelance") ? get_theme_mod("title_freelance") : __('Find perfect freelancers for your projects', ET_DOMAIN); ?></h2>
+		<?php get_template_part('home-list', 'profiles'); ?>
+        <div class="fre-perfect-freelancer-more">
+            <a class="fre-btn-o primary-color"
+               href="<?php echo get_post_type_archive_link(PROFILE); ?>"><?php _e('See all freelancers', ET_DOMAIN); ?></a>
+        </div>
+    </div>
 </div>
 <!-- List Profiles -->
 <!-- List Projects -->
 <div class="fre-jobs-online">
-	<div class="container">
-		<h2 id="title_project"><?php echo get_theme_mod("title_project") ? get_theme_mod("title_project") : __('Browse numerous freelance jobs online', ET_DOMAIN);?></h2>
-		<?php get_template_part( 'home-list', 'projects' );?>
-	</div>
+    <div class="container">
+        <h2 id="title_project"><?php echo get_theme_mod("title_project") ? get_theme_mod("title_project") : __('Browse numerous freelance jobs online', ET_DOMAIN); ?></h2>
+		<?php get_template_part('home-list', 'projects'); ?>
+    </div>
 </div>
 <!-- List Projects -->
 <!-- List Testimonials -->
 <div class="fre-our-stories">
-	<div class="container">
-		<h2 id="title_story"><?php echo get_theme_mod("title_story") ? get_theme_mod("title_story") : __('Hear what our customers have to say', ET_DOMAIN);?></h2>
-		<?php get_template_part( 'home-list', 'testimonial' );?>
-	</div>
+    <div class="container">
+        <h2 id="title_story"><?php echo get_theme_mod("title_story") ? get_theme_mod("title_story") : __('Hear what our customers have to say', ET_DOMAIN); ?></h2>
+		<?php get_template_part('home-list', 'testimonial'); ?>
+    </div>
 </div>
 <!-- List Testimonials -->
 <!-- List Pricing Plan -->
 <?php
 global $disable_plan, $pay_to_bid;
-$disable_plan = (int) ae_get_option( 'disable_plan', false );
-$pay_to_bid = ae_get_option( 'pay_to_bid', false );
+$disable_plan = (int)ae_get_option('disable_plan', false);
+$pay_to_bid   = ae_get_option('pay_to_bid', false);
 
-if( ! $disable_plan || $pay_to_bid ){ ?>
-	<div class="fre-service">
-		<div class="container">
-			<h2 id="title_service">
+if (!$disable_plan || $pay_to_bid) { ?>
+    <div class="fre-service">
+        <div class="container">
+            <h2 id="title_service">
 				<?php
-				if( ae_user_role($user_ID) == FREELANCER ){
+				if (ae_user_role($user_ID) == FREELANCER) {
 					echo get_theme_mod("title_service_freelancer") ? get_theme_mod("title_service_freelancer") : __('Select the level of service you need for project bidding', ET_DOMAIN);
-				}else{
+				} else {
 					echo get_theme_mod("title_service") ? get_theme_mod("title_service") : __('Select the level of service you need for project posting', ET_DOMAIN);
 				}
 				?>
-			</h2>
-			<?php get_template_part( 'home-list', 'pack' );?>
-		</div>
-	</div>
+            </h2>
+			<?php get_template_part('home-list', 'pack'); ?>
+        </div>
+    </div>
 <?php } ?>
 <!-- List Pricing Plan -->
 <!-- List Get Started -->
 <div class="fre-get-started">
-	<div class="container">
-		<div class="get-started-content">
-			<?php if(!is_user_logged_in()){ ?>
-				<h2 id="title_start"><?php echo get_theme_mod("title_start") ? get_theme_mod("title_start") : __('Need work done? Join FreelanceEngine community!', ET_DOMAIN);?></h2>
-				<?php if(fre_check_register()){ ?>
-					<a class="fre-btn fre-btn primary-bg-color" href="<?php echo et_get_page_link('register');?>"><?php _e('Get Started', ET_DOMAIN)?></a>
+    <div class="container">
+        <div class="get-started-content">
+			<?php if (!is_user_logged_in()) { ?>
+                <h2 id="title_start"><?php echo get_theme_mod("title_start") ? get_theme_mod("title_start") : __('Need work done? Join FreelanceEngine community!', ET_DOMAIN); ?></h2>
+				<?php if (fre_check_register()) { ?>
+                    <a class="fre-btn fre-btn primary-bg-color"
+                       href="<?php echo et_get_page_link('register'); ?>"><?php _e('Get Started', ET_DOMAIN) ?></a>
 				<?php } ?>
-			<?php }else{ ?>
-				<?php if(ae_user_role($user_ID) == FREELANCER){ ?>
-					<h2 id="title_start"><?php echo get_theme_mod("title_start_freelancer") ? get_theme_mod("title_start_freelancer") : __("It's time to start finding freelance jobs online!" , ET_DOMAIN);?></h2>
-					<a class="fre-btn fre-btn primary-bg-color" href="<?php echo get_post_type_archive_link( PROJECT ); ?>"><?php _e('Find Projects', ET_DOMAIN)?></a>
-				<?php }else{ ?>
-					<h2 id="title_start"><?php echo get_theme_mod("title_start_employer") ? get_theme_mod("title_start_employer") : __('The best way to find perfect freelancers!', ET_DOMAIN);?></h2>
-					<a class="fre-btn fre-btn primary-bg-color" href="<?php echo et_get_page_link('submit-project'); ?>"><?php _e('Post a Project', ET_DOMAIN)?></a>
+			<?php } else { ?>
+				<?php if (ae_user_role($user_ID) == FREELANCER) { ?>
+                    <h2 id="title_start"><?php echo get_theme_mod("title_start_freelancer") ? get_theme_mod("title_start_freelancer") : __("It's time to start finding freelance jobs online!", ET_DOMAIN); ?></h2>
+                    <a class="fre-btn fre-btn primary-bg-color"
+                       href="<?php echo get_post_type_archive_link(PROJECT); ?>"><?php _e('Find Projects', ET_DOMAIN) ?></a>
+				<?php } else { ?>
+                    <h2 id="title_start"><?php echo get_theme_mod("title_start_employer") ? get_theme_mod("title_start_employer") : __('The best way to find perfect freelancers!', ET_DOMAIN); ?></h2>
+                    <a class="fre-btn fre-btn primary-bg-color"
+                       href="<?php echo et_get_page_link('submit-project'); ?>"><?php _e('Post a Project', ET_DOMAIN) ?></a>
 				<?php } ?>
 			<?php } ?>
 
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
 <!-- List Get Started -->
 <?php get_footer(); ?>
--- wp-content/themes/freelancemarketplacericardo-child/functions.php
+++ wp-content/themes/freelancemarketplacericardo-child/functions.php
@@ -4,11 +4,15 @@ function ricardo_files() {
 	wp_enqueue_style( 'OpenSans', '//fonts.googleapis.com/css?family=Open+Sans:300,400,600' );	 //font-family: 'Open Sans', sans-serif;
 	wp_enqueue_style( 'ricardo_parent_style-1', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'ricardo_parent_styles-2', get_template_directory_uri() . '/css/custom.css' );
+
+
+	//wp_enqueue_style('bootstrapcss', '//stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css');
 	wp_enqueue_style( 'animatecss', '//cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css' );
 	wp_enqueue_style( 'ricardo_child_style', get_stylesheet_uri(), null, microtime() );
 
 
-
+	//wp_enqueue_script( 'properjs', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array('jquery'), null, true );
+	//wp_enqueue_script( 'bootstrapjs', '//stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js', array( 'jquery', 'properjs' ), '4.1', true );
 	wp_enqueue_script( 'waypoint', get_theme_file_uri('/js/jquery.waypoints.min.js'), array('jquery'), null, true );
 	wp_enqueue_script( 'connectslice_mainjs', get_theme_file_uri('/js/main.js'), array('jquery'), '1.0', true );
 
